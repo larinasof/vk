da5f2ff0ed9f8f5ea77e57cc158c6de923b8c128
Sofia Larina <zazaq97@gmail.com>
2020-08-06 11:17:48 +0300

    add manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,25 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] wallSearch(){
+        int ownerId;
+        String domain;
+        String query;
+        boolean ownersOnly;
+        int count;
+        int offset;
+        boolean extended;
+        String fields;
+
+        return null;
+    }
+
+    public void wallDelete(){
+        int ownerId;
+        int postId;
+    }
+}
