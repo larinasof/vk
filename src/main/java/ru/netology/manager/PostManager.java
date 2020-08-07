439241fb7433cd792c7a5375075371d57c47ed6b
Sofia Larina <zazaq97@gmail.com>
2020-08-07 09:20:57 +0300

    syntax changed

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -5,12 +5,12 @@ import ru.netology.domain.Post;
 public class PostManager {
     private Post[] posts;
 
-    public Post[] wallSearch(int ownerId; String domain; String query; boolean ownersOnly;
-    int count; int offset;){
+    public Post[] wallSearch(int ownerId, String domain, String query, boolean ownersOnly,
+    int count, int offset){
 
         return null;
     }
 
-    public void wallDelete(int ownerId; int postId){
+    public void wallDelete(int ownerId, int postId){
     }
 }
